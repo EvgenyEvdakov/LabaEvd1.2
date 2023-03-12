e23fd8466a4e85634d9e295a3730987c57e69621
Evgeny <kkrutkov02@gmail.com>
2023-03-12 15:46:21 +0300

    scanf a, b, c, x

--- programm.cpp
+++ programm.cpp
@@ -7,7 +7,11 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	printf("¬ведите значение а: ");
+	scanf_s("%d", &a);
 	printf("¬ведите значение b: ");
+	scanf_s("%d", &b);
 	printf("¬ведите значение c: ");
+	scanf_s("%d", &c);
 	printf("¬ведите значение d: ");
+	scanf_s("%d", &x);
 }
